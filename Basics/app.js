03c665cfd4dc7ab844705030160c70d684d38888
Jihirsh <jihirshsingh12@gmail.com>
2025-03-24 03:24:44 +0530

    learning git and github

--- Basics/app.js
+++ Basics/app.js
@@ -6,3 +6,72 @@ let firstName = "Jihirsh";
 console.log(firstName);
 
 console.log(firstName.slice(0, 4));
+
+//do-while loop
+// let i = 20;
+// do {
+//     console.log(firstName, i);
+//     i++;
+// } while (i <= 400);
+
+
+//arrays
+let myList = ["Eat", "Sleep", "Code", "Repeat"];
+console.log(myList[2]);
+
+const person = {
+    name: "Jihirsh",
+    age: 22,
+    location: "India",
+    isStudent: true,
+};
+
+person.lastName = "Singh";
+console.log(person);
+
+
+//Function Declaration
+function greet(name) {
+    console.log(`Hello ${name} from our first function`);
+}
+//calling the function
+greet("Jihirsh");
+greet("Anna")
+
+
+function add(a, b, c) {
+    return a + b + c;
+}
+console.log(add(17, 34, 113));
+
+//Function Expression
+const square = function (x) {
+    return x * x;
+};
+console.log(square(5)); //don't put this line before initializing the function expression
+
+
+//When we provide function as an argument to another function, that function is called a callback function
+function greetings(nameNew, callback) {
+    console.log("Hello, " + nameNew);
+    callback();
+}
+
+function sayGoodbye() {
+    console.log("Goodbye!");
+}
+
+// Passing `sayGoodbye` as a callback to `greet`
+greetings("Alice", sayGoodbye);
+// Output:
+// Hello, Alice
+// Goodbye!
+
+
+//Scope in JavaScript refers to the current context of code, which determines the accessibility of variables to JavaScript.
+//The two types of scope are local and global:
+//Local variables are declared within a function and can only be accessed within that function.
+//Global variables are declared outside of a function and can be accessed throughout the program.
+
+const currentDate = new Date();
+console.log(currentDate);

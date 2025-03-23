59d1848d5fab9aeff2224549af55bf56b7529b81
Jihirsh <jihirshsingh12@gmail.com>
2025-03-23 18:23:48 +0530

    new commit

--- Basics/app.js
+++ Basics/app.js
@@ -1,1 +1,8 @@
 console.log("Hello Jihirsh");
+alert ("Hello Jihirsh");
+
+// This is a comment : clg -> console.log (short form in VS Code)
+let firstName = "Jihirsh";
+console.log(firstName);
+
+console.log(firstName.slice(0, 4));

0246c863bb901facca758ccbe8d1c9c2613f92df
Jihirsh <jihirshsingh12@gmail.com>
2025-03-29 02:18:15 +0530

    late night

--- Parttwo/app.js
+++ Parttwo/app.js
@@ -42,4 +42,51 @@ setTimeout(() => {
             }, 2000);
         }, 2000);
     }, 2000);
-}, 2000); 
+}, 2000);
+
+function ratings(rate) {
+    if (rate === 5) {
+        console.log(`High Ratings :)`);
+    } else if (rate === 0) {
+        console.log(`Low Ratings :(`);
+    }
+}
+ratings(0);
+
+function multiply(a, b=1, c=10) {
+    return a * b * c;
+}
+console.log(multiply(2, 2));  //output - 40
+
+function giveMe4(a, b, c, d) {
+    console.log("a", a);
+    console.log("b", b);
+    console.log("c", c);
+    console.log("d", d);
+}
+
+colors = ["red", "green", "blue", "yellow"];
+giveMe4(...colors); //output - a red, b green, c blue, d yellow
+
+let people = ["huxn", "alex", "jordan"];
+const allPeps = ["kumar", ...people, "james", "john"];
+console.log(allPeps); //output - kumar huxn alex jordan james john
+
+//make functions with a lot of parameters
+function user(x, ...userData) { //x cannot be written after userData or will throw error
+    console.log(x);
+    console.log(userData);
+}
+user("Jihirsh", 21, "India", "Delhi", "Software Developer", "Football");
+
+function person(firstName, lastName, ...hobbies) {
+    console.log("First Name: ", firstName);
+    console.log("Second Name: ", lastName);
+    console.log("Hobbies: ", hobbies);
+}
+
+person("Jihirsh", "Singh", "Football", "Physics", "Coding", "Reading", "Flute");
+
+
+//Destructuring is a JavaScript expression that makes it possible to unpack values from arrays, or properties from objects, into distinct variables.
+//It is a convenient way to extract values from arrays or properties from objects into distinct variables.

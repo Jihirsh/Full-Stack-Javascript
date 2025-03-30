83888b66424a3bdfac0ab340dcbef622d28be7b9
Jihirsh <jihirshsingh12@gmail.com>
2025-03-30 22:31:24 +0530

    let's finish tonight

---
+++ 02 Accessing the DOM/live-coding/app.js
@@ -0,0 +1,9 @@
+//getElementsByTagName
+console.log(document.getElementsByTagName("h1"));
+console.log(document.getElementsByTagName("h1").length);
+
+//getElementsById
+console.log(document.getElementById("main"));
+
+//getElementsByClassName
+console.log(document.getElementsByClassName("cls-1"));
